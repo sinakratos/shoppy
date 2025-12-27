4b484145d6518d0c23abbb261c49c76e45530595
sinakratos <sinakhoshzaban2@gmail.com>
2025-12-27 19:07:50 +0330

    Create user with prisma

--- shoppy-backend/src/users/users.service.ts
+++ shoppy-backend/src/users/users.service.ts
@@ -1,7 +1,28 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnprocessableEntityException } from '@nestjs/common';
 import { CreateUsersRequest } from './dto/create-user.request';
-
+import { PrismaService } from 'src/prisma/prisma.service';
+import { User } from 'generated/prisma/browser';
+import * as bcrypt from 'bcrypt';
 @Injectable()
 export class UsersService {
-  createUser(data: CreateUsersRequest) {}
+  constructor(private readonly prismaService: PrismaService) {}
+
+  async createUser(data: CreateUsersRequest) {
+    try {
+      return await this.prismaService.user.create({
+        data: {
+          ...data,
+          password: await bcrypt.hash(data.password, 10),
+        },
+        select: { id: true, email: true },
+      });
+    } catch (err: any) {
+      if (err.code === 'P2002') {
+        throw new UnprocessableEntityException(
+          'User with this email already exists',
+        );
+      }
+      throw err;
+    }
+  }
 }
